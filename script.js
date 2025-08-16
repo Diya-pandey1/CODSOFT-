addc371df8c2cb96671e4a299ca227a9be48e585
DIYA PANDEY <diyapandey1828@gmail.com>
2025-08-16 13:46:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.buttons button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.textContent;
+
+        if( button.classList.contains ('clear')) {
+            display.value ='';
+
+        } else if (value === 'DEL'){
+            
+            display,value=display.value.slice(0, -1);    
+        } else if (value ==='=') {
+            try {
+                display.value = eval(display.value);
+            } catch  {
+                display.value = 'Error';
+            }
+        }else {
+            display.value += value;
+        }
+    });
+});
+
+    
